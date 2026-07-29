7ecfe5783ebc6f63c9ac204efb636043c18b38c6
Jalal Ali <alikhanumar795@gmail.com>
2026-07-29 18:53:52 +0500

    Update axiosInstance.js

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -9,7 +9,7 @@ const api = axios.create({
 
 api.interceptors.request.use((config) => {
     const token = localStorage.getItem("token");
-    if(token){
+    if (token) {
         config.headers.Authorization = `Bearer ${token}`;
     }
     return config
@@ -17,26 +17,27 @@ api.interceptors.request.use((config) => {
 api.interceptors.response.use((response) => {
     return response
 },
-async (error) =>{
-    const originalReq = error.config;
-    if(error.response?.status == 401 &&
-         error.response?.data?.code == "TOKEN_EXPIRED"){
-            console.log("Token Expired", "Code:", error.response?.data?.code, "Status:" , error.response?.status );
-            
-            try{
+    async (error) => {
+        const originalReq = error.config;
+        if (error.response?.status == 401 &&
+            error.response?.data?.code == "TOKEN_EXPIRED" &&
+            !originalReq._retry) {
+            originalReq._retry = true;
+            try {
                 const res = await api.post("/refresh");
                 const newToken = res.data.token;
-                console.log("Token Refreshed", newToken);                
+                console.log("Token Refreshed");
                 localStorage.setItem("token", newToken);
                 originalReq.headers.Authorization = `Bearer ${newToken}`
                 return api(originalReq)
-            }catch(err){
+            } catch (err) {
                 return Promise.reject(err)
             }
-    }
-    return Promise.reject(error);
-    
-})
+
+        }
+        return Promise.reject(error);
+
+    })
 
 export { api }
 
